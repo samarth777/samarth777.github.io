f8956ec098a3abb413b90f5712e89b6a759d261a
samarth777 <63744278+samarth777@users.noreply.github.com>
2025-10-05 15:40:02 +0000

    Deploying to gh-pages from @ samarth777/samarth777.github.io@ad437b524f2367f45f9ad70e7204c2075770812b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "samarth777/ai-folio",
+    "data-repo": "samarth777/samarth777.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
