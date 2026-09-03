262ba6f8bfe4f2854be5d4c22fd822c67883e635
samarth777 <63744278+samarth777@users.noreply.github.com>
2026-09-03 10:32:31 +0000

    Deploying to gh-pages from @ samarth777/samarth777.github.io@9257b7b4c9cd501757b5f9af70bfd91a5cd34a85 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -186,13 +186,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/samarth-prakash", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
